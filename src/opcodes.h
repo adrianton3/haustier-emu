0690984a8b64f0ea43575f120a48656fe3ff672b
Adrian Toncean <adrianton3@gmail.com>
2025-09-15 17:55:27 +0200

    add all instructions with implied addressing mode

--- src/opcodes.h
+++ src/opcodes.h
@@ -27,9 +27,31 @@ struct std::hash<InsAndMode> {
 };
 
 static std::unordered_map<InsAndMode, uint8_t> opcodes {
-    { { "BRK", AddressingMode::Implied } , 0x00 },
-    { { "RTI", AddressingMode::Implied } , 0x40 },
-    { { "RTS", AddressingMode::Implied } , 0x60 },
+    { { "BRK", AddressingMode::Implied }, 0x00 },
+    { { "CLC", AddressingMode::Implied }, 0x18 },
+    { { "CLD", AddressingMode::Implied }, 0xD8 },
+    { { "CLI", AddressingMode::Implied }, 0x58 },
+    { { "CLV", AddressingMode::Implied }, 0xB8 },
+    { { "DEI", AddressingMode::Implied }, 0x78 },
+    { { "DEX", AddressingMode::Implied }, 0xC9 },
+    { { "DEY", AddressingMode::Implied }, 0x88 },
+    { { "INX", AddressingMode::Implied }, 0xE8 },
+    { { "INY", AddressingMode::Implied }, 0xC8 },
+    { { "NOP", AddressingMode::Implied }, 0xE9 },
+    { { "PHA", AddressingMode::Implied }, 0x48 },
+    { { "PHP", AddressingMode::Implied }, 0x08 },
+    { { "PLA", AddressingMode::Implied }, 0x68 },
+    { { "PLP", AddressingMode::Implied }, 0x28 },
+    { { "RTI", AddressingMode::Implied }, 0x40 },
+    { { "RTS", AddressingMode::Implied }, 0x60 },
+    { { "SEC", AddressingMode::Implied }, 0x38 },
+    { { "SED", AddressingMode::Implied }, 0xF8 },
+    { { "TAX", AddressingMode::Implied }, 0xA9 },
+    { { "TAY", AddressingMode::Implied }, 0xA8 },
+    { { "TSX", AddressingMode::Implied }, 0xB9 },
+    { { "TXA", AddressingMode::Implied }, 0x89 },
+    { { "TXS", AddressingMode::Implied }, 0x99 },
+    { { "TYA", AddressingMode::Implied }, 0x98 },
 };
 
 
